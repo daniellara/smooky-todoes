877aba277316f4760c26902f9672af348a8e7e7c
Daniel Lara <daniel.lara.diezma@gmail.com>
2017-08-30 14:41:40 +0200

    First commit

---
+++ server/db/dbHandler.js
@@ -0,0 +1,74 @@
+const {ObjectID} = require('mongodb');
+
+const
+  {Todo} = require('../models/todo'),
+  {User} = require('../models/user');
+
+function findTodos() {
+  return new Promise((resolve, reject) => {
+    Todo.find().then((todos) => {
+      resolve({
+        data: todos
+      })
+    }).catch((err) => {
+      console.error('An error happened in findTodos.', err);
+      reject({
+        code: 400,
+        message: 'Cannot find todos'
+      })
+    })
+  });
+};
+
+function saveTodo(todoText) {
+  let todo = new Todo({
+    text: todoText
+  });
+  return new Promise((resolve, reject) => {
+    todo.save().then((doc) => {
+      resolve({
+        data: doc
+      });
+    }).catch((err) => {
+      console.error('An error happened in saveTodo.', err);
+      reject({
+        code: 400,
+        message: 'Cannot save the todo'
+      })
+    });
+  });
+};
+
+function findTodo(id) {
+  return new Promise((resolve, reject) => {
+    if (!ObjectID.isValid(id)) {
+      return reject({
+        code: 400,
+        message: 'ID not valid'
+      })
+    }
+    Todo.findById(id).then((todo) => {
+      if (!todo) {
+        return reject({
+          code: 404,
+          message: 'ID not found'
+        });
+      }
+      resolve({
+        data: todo
+      });
+    }).catch((err) => {
+      console.error('An error happened in findTodo.', err);
+      reject({
+        code: 400,
+        message: 'An error ocurred fetching the todo'
+      })
+    })
+  });
+}
+
+module.exports = {
+  findTodos,
+  saveTodo,
+  findTodo
+}
---
+++ server/server.js
@@ -0,0 +1,49 @@
+const
+  express = require('express'),
+  bodyParser = require('body-parser');
+
+const
+  {mongoose} = require('./db/mongoose'),
+  dbHandler = require('./db/dbHandler');
+
+let app = express();
+
+app.use(bodyParser.json());
+
+app.route('/todos')
+  .get((req, res) => {
+    dbHandler.findTodos().then((value) => {
+      res.send({
+        todos: value.data
+      });
+    }).catch((err) => {
+      console.error('There was an error fetching the todos.', err);
+      res.status(err.code).send({message: err.message});
+    });
+  })
+  .post((req, res) => {
+    dbHandler.saveTodo(req.body.text).then((value) => {
+      res.send(value.data);
+    }).catch((err) => {
+      console.error('There was an error saving the todo.', err);
+      res.status(err.code).send({message: err.message});
+    });
+  });
+
+app.route('/todos/:id')
+  .get((req, res) => {
+    dbHandler.findTodo(req.params.id).then((value) => {
+      res.send({
+        todo: value.data
+      });
+    }).catch((err) => {
+      console.error('There was an error fetching the todo.', err);
+      res.status(err.code).send({message: err.message});
+    })
+  })
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
+
+module.exports = {app};
