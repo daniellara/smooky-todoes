83918f1db873801f18113f20e93e3810a6cfa850
Daniel Lara <daniel.lara.diezma@gmail.com>
2017-09-12 15:01:28 +0200

    Add update todo endpoint

--- server/db/dbHandler.js
+++ server/db/dbHandler.js
@@ -1,5 +1,6 @@
 const { ObjectID } = require('mongodb');
 const { Todo } = require('../models/todo');
+const _ = require('lodash');
 
 function findTodos() {
   return new Promise((resolve, reject) => {
@@ -89,9 +90,44 @@ function removeTodo(id) {
   });
 }
 
+function updateTodo(id, body) {
+  return new Promise((resolve, reject) => {
+    if (!ObjectID.isValid(id)) {
+      return reject({
+        code: 400,
+        message: 'ID not valid',
+      });
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+      body.completedAt = new Date().getTime();
+    } else {
+      body.completed = false;
+      body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+      if (!todo) {
+        return reject({
+          code: 404,
+          message: 'ID not found',
+        });
+      }
+      return resolve({
+        data: todo,
+      });
+    }).catch(err => reject({
+      code: 400,
+      message: 'An error happened removing the todo',
+      errObj: err,
+    }));
+  });
+}
+
 module.exports = {
   findTodos,
   saveTodo,
   findTodo,
   removeTodo,
+  updateTodo,
 };
--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('./db/mongoose');
 const dbHandler = require('./db/dbHandler');
 
+const _ = require('lodash');
+
 mongoose.connectToMongoDB();
 
 const app = express();
@@ -41,6 +43,17 @@ app.route('/todos/:id')
   })
   .delete((req, res) => {
     dbHandler.removeTodo(req.params.id).then((value) => {
+      res.send({
+        todo: value.data,
+      });
+    }).catch((err) => {
+      res.status(err.code).send({ message: err.message });
+    });
+  })
+  .patch((req, res) => {
+    const id = req.params.id;
+    const body = _.pick(req.body, ['text', 'completed']);
+    dbHandler.updateTodo(id, body).then((value) => {
       res.send({
         todo: value.data,
       });
