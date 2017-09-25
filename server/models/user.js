7fcfac9a19c9ef5c8279a2656d20887e0f50da42
Daniel Lara <daniel.lara.diezma@gmail.com>
2017-09-25 16:18:05 +0200

    Update User model

---
+++ server/models/user.js
@@ -0,0 +1,37 @@
+/**
+ * Mongoose model for User
+ */
+const mongoose = require('mongoose');
+
+const val = require('validator');
+
+const User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: val.isEmail,
+      message: '{VALUE} is not defined'
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  token: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
+});
+
+module.exports = { User };
